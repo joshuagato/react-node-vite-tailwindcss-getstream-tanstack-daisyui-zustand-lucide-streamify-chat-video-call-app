392b8dfc353c254251f1002138f866f92c3703d6
Joshua Gato <joshuagato37@gmail.com>
2025-12-14 21:58:13 +0000

    Reformatted file

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import {
+    login,
+    logout,
+    onboard,
+    signup,
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -12,7 +17,7 @@ router.post("/onboarding", protectRoute, onboard);
 
 // check if user is logged in
 router.get("/me", protectRoute, (req, res) => {
-  res.status(200).json({ success: true, user: req.user });
+    res.status(200).json({ success: true, user: req.user });
 });
 
 export default router;
