5b5470fd314067900845bbfec7de58d2a149dcea
Joshua Gato <joshuagato37@gmail.com>
2025-12-16 19:07:02 +0000

    Added conditional rendering of api route

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+    import.meta.env.MODE === "development"
+        ? import.meta.env.VITE_API_URL
+        : "/api";
+
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: BASE_URL,
     withCredentials: true, // send cookies with the request
 });
